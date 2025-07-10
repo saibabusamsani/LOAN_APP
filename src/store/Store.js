a074287190c93e6b3f9eab4381e8885e9035827e
saibabu <saibabu@leadwinner.com>
2025-07-10 13:26:38 +0530

    added store

---
+++ src/store/Store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { themeReducer } from "./themeSlice";
+import { authenticationReducer } from "./AuthenticationSlice";
+
+const store = configureStore({
+  reducer: {
+    theme: themeReducer,
+    authentication:authenticationReducer
+  },
+});
+
+export default store;
